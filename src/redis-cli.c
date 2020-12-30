d99b7fd4425e8a59364268c35b6e41c558c7a2a5
neohope <neohope@yahoo.com>
2020-12-31 00:16:51 +0800

    remove replication and sentinel

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -925,6 +925,83 @@ static int issueCommand(int argc, char **argv) {
     return issueCommandRepeat(argc, argv, config.repeat);
 }
 
+static void repl(void) {
+	sds historyfile = NULL;
+	int history = 0;
+	char *line;
+	int argc;
+	sds *argv;
+
+	config.interactive = 1;
+	linenoiseSetMultiLine(1);
+	linenoiseSetCompletionCallback(completionCallback);
+
+	/* Only use history when stdin is a tty. */
+	if (isatty(fileno(stdin))) {
+		historyfile = getHistoryPath();
+		if (historyfile != NULL) {
+			history = 1;
+			linenoiseHistoryLoad(historyfile);
+		}
+	}
+
+	cliRefreshPrompt();
+	while ((line = linenoise(context ? config.prompt : "not connected> ")) != NULL) {
+		if (line[0] != '\0') {
+			argv = sdssplitargs(line, &argc);
+			if (history) linenoiseHistoryAdd(line);
+			if (historyfile) linenoiseHistorySave(historyfile);
+
+			if (argv == NULL) {
+				printf("Invalid argument(s)\n");
+				free(line);
+				continue;
+			}
+			else if (argc > 0) {
+				if (strcasecmp(argv[0], "quit") == 0 ||
+					strcasecmp(argv[0], "exit") == 0)
+				{
+					exit(0);
+				}
+				else if (argc == 3 && !strcasecmp(argv[0], "connect")) {
+					sdsfree(config.hostip);
+					config.hostip = sdsnew(argv[1]);
+					config.hostport = atoi(argv[2]);
+					cliRefreshPrompt();
+					cliConnect(1);
+				}
+				else if (argc == 1 && !strcasecmp(argv[0], "clear")) {
+					linenoiseClearScreen();
+				}
+				else {
+					PORT_LONGLONG start_time = mstime(), elapsed;
+					int repeat, skipargs = 0;
+
+					repeat = atoi(argv[0]);
+					if (argc > 1 && repeat) {
+						skipargs = 1;
+					}
+					else {
+						repeat = 1;
+					}
+
+					issueCommandRepeat(argc - skipargs, argv + skipargs, repeat);
+
+					elapsed = mstime() - start_time;
+					if (elapsed >= 500) {
+						printf("(%.2fs)\n", (double)elapsed / 1000);
+					}
+				}
+			}
+			/* Free the argument vector */
+			sdsfreesplitres(argv, argc);
+		}
+		/* linenoise() returns malloc-ed lines like readline() */
+		free(line);
+	}
+	exit(0);
+}
+
 static int noninteractive(int argc, char **argv) {
     int retval = 0;
     if (config.stdinarg) {
@@ -2107,7 +2184,7 @@ int main(int argc, char **argv) {
         /* Note that in repl mode we don't abort on connection error.
          * A new attempt will be performed for every command send. */
         cliConnect(0);
-        //repl();
+        repl();
     }
 
     /* Otherwise, we have some arguments to execute */
